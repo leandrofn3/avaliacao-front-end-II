8e60ade447690ae8febb76c18666094dd95199e0
leandrofn3 <lf1938833@gmail.com>
2023-06-22 22:50:11 -0300

    arrumando o bug da barra de pesquisar e arrumando os cards

--- js/index.js
+++ js/index.js
@@ -36,15 +36,15 @@ function getCharacters() {
                         const lastEpisodeName = lastEpisode.name;
 
                         cards.innerHTML += ` <div class="col-4">
-                        <div class="card">
+                        <div class="card bg-transparent">
                             <img src="${character.image}" class="card-img-top" alt="">
-                                <div class="card-body">
-                                <h2>${character.name}</h2>
-                                <div class="pTextStatus"><div class="statusColor ${statusColorClass}"></div> ${character.status} - ${character.species}</div>
-                                <p class="pTitulo">last known location:</p>
-                                <p class="pText">${character.location.name}</p>
-                                <p class="pTitulo">Last Episode:</p>
-                                <p class="pText">${lastEpisodeName}</p>
+                                <div class="card-body ">
+                                <h2 class="text-light">${character.name}</h2>
+                                <div class="pTextStatus text-light"><div class="statusColor ${statusColorClass}"></div><b>${character.status} - ${character.species}</b></div>
+                                <div class="textCard"><p class="pTitulo text-secondary"><b>last known location:</b></p>
+                                <p class="pText text-light"><b>${character.location.name}</b></p></div>
+                                <div class="textCard"><p class="pTitulo text-secondary "><b>Last Episode:</b></p>
+                                <p class="pText text-light"><b>${lastEpisodeName}</b></p></div>
                             </div>
                         </div>
                         `;
